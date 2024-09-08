2e26c166e0e33b867aee47d4092717423335ba3a
rafaelrmb <rafaelmotermb@gmail.com>
2024-09-07 22:59:40 -0300

    feat: add function to convert text nodes to html nodes

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,6 @@
+from leafnode import LeafNode
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -5,7 +8,6 @@ class TextNode:
         self.url = url
 
     def __eq__(self, target: object) -> bool:
-        print(self.__class__.__name__)
         if not isinstance(target, TextNode):
             return False
 
@@ -17,3 +19,32 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.text}, {self.text_type}, {self.url})"
+
+    def text_node_to_html_node(text_node):
+        types_dict = {
+            "text": None,
+            "bold": "b",
+            "italic": "i",
+            "code": "code",
+            "link": "a",
+            "image": "img",
+        }
+
+        if text_node.text_type not in types_dict:
+            raise ValueError(
+                "Invalid text type. Valid types are: " + " ".join(types_dict.keys())
+            )
+
+        if text_node.text_type == "link":
+            return LeafNode(
+                text_node.text, types_dict[text_node.text_type], {"href": text_node.url}
+            )
+
+        if text_node.text_type == "image":
+            return LeafNode(
+                "",
+                types_dict[text_node.text_type],
+                {"src": text_node.url, "alt": text_node.text},
+            )
+
+        return LeafNode(text_node.text, types_dict[text_node.text_type])
