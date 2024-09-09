8e2fdc8b912bd1e0021e80eacbd564996efca01e
rafaelrmb <rafaelmotermb@gmail.com>
2024-09-09 18:40:04 -0300

    refactor: move local variable to object attribute

--- src/textnode.py
+++ src/textnode.py
@@ -6,6 +6,14 @@ class TextNode:
         self.text = text
         self.text_type = text_type
         self.url = url
+        self.types_dict = {
+            "text": None,
+            "bold": "b",
+            "italic": "i",
+            "code": "code",
+            "link": "a",
+            "image": "img",
+        }
 
     def __eq__(self, target: object) -> bool:
         if not isinstance(target, TextNode):
@@ -20,31 +28,25 @@ class TextNode:
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.text}, {self.text_type}, {self.url})"
 
-    def text_node_to_html_node(text_node):
-        types_dict = {
-            "text": None,
-            "bold": "b",
-            "italic": "i",
-            "code": "code",
-            "link": "a",
-            "image": "img",
-        }
-
-        if text_node.text_type not in types_dict:
+    def text_node_to_html_node(self, text_node):
+        if text_node.text_type not in self.types_dict:
             raise ValueError(
-                "Invalid text type. Valid types are: " + " ".join(types_dict.keys())
+                "Invalid text type. Valid types are: "
+                + " ".join(self.types_dict.keys())
             )
 
         if text_node.text_type == "link":
             return LeafNode(
-                text_node.text, types_dict[text_node.text_type], {"href": text_node.url}
+                text_node.text,
+                self.types_dict[text_node.text_type],
+                {"href": text_node.url},
             )
 
         if text_node.text_type == "image":
             return LeafNode(
                 "",
-                types_dict[text_node.text_type],
+                self.types_dict[text_node.text_type],
                 {"src": text_node.url, "alt": text_node.text},
             )
 
-        return LeafNode(text_node.text, types_dict[text_node.text_type])
+        return LeafNode(text_node.text, self.types_dict[text_node.text_type])
